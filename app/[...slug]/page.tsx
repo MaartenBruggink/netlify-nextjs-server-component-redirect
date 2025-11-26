64b849bdb5c56b5fc5c72d70e24a9096795df625
Maarten Bruggink <maarten@obelism.studio>
2025-11-26 13:59:34 +0100

    🔨 - Throw error

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,10 +1,14 @@
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 const EXPECTED_SLUG_ARR = ["test", "123"];
 const EXPECTED_SLUG = EXPECTED_SLUG_ARR.join("/");
 
 const Page = async (context: PageProps<"/[...slug]">) => {
 	const params = await context.params;
+
+	if (params.slug.length > 2) {
+		return notFound();
+	}
 
 	if (params.slug.join("/") !== EXPECTED_SLUG) {
 		return redirect(`/${EXPECTED_SLUG}`);
